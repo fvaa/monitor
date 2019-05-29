a7254aaf0b09d21e3a7e0d8a169ff327b9c99751
evio <evio@vip.qq.com>
2019-05-29 19:09:43 +0800

    first commit

---
+++ build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Monitor(options = {
+    prefix: '/',
+    mode: 'hash'
+}) {
+}
+exports.default = Monitor;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+interface MonitorArguments {
+  prefix?: string,
+  mode?: string
+}
+
+export default function Monitor(options: MonitorArguments = {
+  prefix: '/',
+  mode: 'hash'
+}) {
+  
+}
